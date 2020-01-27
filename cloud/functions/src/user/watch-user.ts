afb477f1d6bbe13c77586b0ccbe4fcb045816d2e
peterpeterparker <david.dalbusco@outlook.com>
2020-01-27 08:25:48 +0100

    feat: clone flats to users data

--- cloud/functions/src/user/watch-user.ts
+++ cloud/functions/src/user/watch-user.ts
@@ -4,6 +4,7 @@ import * as admin from 'firebase-admin';
 import {Flat} from '../model/flat';
 
 import {add, findAll} from '../utils/flats.utils';
+import {UserData} from '../model/user';
 
 export async function watchUserCreate(userRecord: admin.auth.UserRecord, _context: EventContext) {
     await cloneFlats(userRecord);
@@ -22,11 +23,32 @@ async function cloneFlats(userRecord: admin.auth.UserRecord) {
         }
 
         const promises: Promise<void>[] = flats.map((flat: Flat) => {
-            return add(`/flats/${userRecord.uid}/`, flat.data);
+            return add(`/users/${userRecord.uid}/flats/`, flat.data);
         });
 
         await Promise.all(promises);
+
+        await create(userRecord);
     } catch (err) {
         console.error(err);
     }
+}
+
+function create(userRecord: admin.auth.UserRecord): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+        try {
+            const documentReference: admin.firestore.DocumentReference = admin.firestore().collection('/users/').doc(userRecord.uid);
+
+            const userData: UserData = {
+                created_at: admin.firestore.Timestamp.now(),
+                updated_at: admin.firestore.Timestamp.now(),
+            };
+
+            await documentReference.set(userData, {merge: true});
+
+            resolve();
+        } catch (err) {
+            reject(err);
+        }
+    });
 }
