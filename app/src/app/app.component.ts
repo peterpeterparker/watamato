9f8917688fe4b33aa8b67779684e1cb6939e1bd8
peterpeterparker <david.dalbusco@outlook.com>
2020-01-28 22:22:03 +0100

    feat: delete button instead of column dislike

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -7,8 +7,8 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 import {AuthService} from './services/auth/auth.service';
 import {UserService} from './services/user/user.service';
+
 import {FlatsNewService} from './services/flats/flats.new.service';
-import {FlatsDislikedService} from './services/flats/flats.disliked.service';
 import {FlatsAppliedService} from './services/flats/flats.applied.service';
 import {FlatsViewingService} from './services/flats/flats.viewing.service';
 import {FlatsRejectedService} from './services/flats/flats.rejected.service';
@@ -26,7 +26,6 @@ export class AppComponent implements OnInit, OnDestroy {
         private statusBar: StatusBar,
         private authService: AuthService,
         private flatsNewService: FlatsNewService,
-        private flatsDislikedService: FlatsDislikedService,
         private flatsAppliedService: FlatsAppliedService,
         private flatsViewingService: FlatsViewingService,
         private flatsRejectedService: FlatsRejectedService,
@@ -48,7 +47,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
         const promises: Promise<void>[] = [
             this.flatsNewService.init(),
-            this.flatsDislikedService.init(),
             this.flatsAppliedService.init(),
             this.flatsViewingService.init(),
             this.flatsRejectedService.init(),
