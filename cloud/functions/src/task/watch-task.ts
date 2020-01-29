7fee8ab60a925c1c4af6ac4c1a0784ee8041de5a
peterpeterparker <david.dalbusco@outlook.com>
2020-01-29 14:28:57 +0100

    feat: do all

--- cloud/functions/src/task/watch-task.ts
+++ cloud/functions/src/task/watch-task.ts
@@ -1,29 +1,47 @@
-import {DocumentSnapshot} from 'firebase-functions/lib/providers/firestore';
-import {EventContext} from 'firebase-functions';
+import { DocumentSnapshot } from "firebase-functions/lib/providers/firestore";
+import { EventContext } from "firebase-functions";
 
-import {FlatData} from '../model/flat';
+import { FlatData } from "../model/flat";
 
-import {crawlRonorp} from '../crawler/ronorp';
+import { crawlRonorp } from "../crawler/ronorp";
+import { crawlHomegate } from "../crawler/homegate";
 
-import {save} from '../utils/flats.utils';
-import {successful} from '../utils/tasks.utils';
+import { save } from "../utils/flats.utils";
+import { successful } from "../utils/tasks.utils";
 
-export async function watchTaskCreate(snapshot: DocumentSnapshot, context: EventContext) {
-    try {
-        const taskId: string = context.params.taskId;
+export async function watchTaskCreate(
+  snapshot: DocumentSnapshot,
+  context: EventContext
+) {
+  try {
+    const taskId: string = context.params.taskId;
 
-        if (!taskId || taskId === undefined || taskId === '') {
-            return;
-        }
-
-        const elements: FlatData[] | undefined = await crawlRonorp();
-
-        await save(elements);
-
-        await successful(taskId);
-
-        console.log(`Crawl done. ${elements ? elements.length : 0} elements found.`);
-    } catch (err) {
-        console.error(err);
+    if (!taskId || taskId === undefined || taskId === "") {
+      return;
     }
+
+    let elements: FlatData[] = [];
+
+    const ronorpElements: FlatData[] | undefined = await crawlRonorp();
+    if (ronorpElements !== undefined) {
+      elements = [...ronorpElements];
+    }
+
+    const homegateElements: FlatData[] | undefined = await crawlHomegate();
+    if (homegateElements !== undefined) {
+      elements = [...elements, ...homegateElements];
+    }
+
+    await save(elements);
+
+    await successful(taskId);
+
+    console.log(
+      `Crawl done. ${
+        ronorpElements ? ronorpElements.length : 0
+      } elements found.`
+    );
+  } catch (err) {
+    console.error(err);
+  }
 }
