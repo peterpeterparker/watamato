916bf270ed6c6a67ab2fb46d993e2ec7d01b379d
peterpeterparker <david.dalbusco@outlook.com>
2020-01-30 08:25:48 +0100

    feat: load version

--- app/scripts/config.index.ts
+++ app/scripts/config.index.ts
@@ -1,4 +1,5 @@
 import {writeFile} from 'fs';
+import {name, version} from '../package.json';
 
 const targetPath = './src/environments/environment.prod.ts';
 
@@ -13,10 +14,12 @@ const envConfigFile = `export const environment = {
         messagingSenderId: '186771088317',
         appId: '1:186771088317:web:431aec6d0cade214d4e8d4',
         measurementId: 'G-XY72KL25LS'
-    }
+    },
+    name: '${name}',
+    version: '${version}'
 };
 `;
 
 writeFile(targetPath, envConfigFile, 'utf8', (err) => {
-    if (err) return console.log(err);
+  if (err) return console.log(err);
 });
--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,8 +1,15 @@
-// Will be overwritten by build
-
-import {firebase} from './firebase.environment';
-
 export const environment = {
   production: true,
-  ...firebase
+  firebase: {
+    apiKey: 'undefined',
+    authDomain: 'watamato-app.firebaseapp.com',
+    databaseURL: 'https://watamato-app.firebaseio.com',
+    projectId: 'watamato-app',
+    storageBucket: 'watamato-app.appspot.com',
+    messagingSenderId: '186771088317',
+    appId: '1:186771088317:web:431aec6d0cade214d4e8d4',
+    measurementId: 'G-XY72KL25LS'
+  },
+  name: 'watamato',
+  version: '0.0.1'
 };
