faa2fe5681e88948d67d34aaca027f40772b1899
peterpeterparker <david.dalbusco@outlook.com>
2020-01-30 07:13:18 +0100

    feat: filter plz

--- cloud/functions/src/crawler/homegate.ts
+++ cloud/functions/src/crawler/homegate.ts
@@ -4,7 +4,7 @@ import { JSDOM } from "jsdom";
 
 import { FlatData } from "../model/flat";
 
-import { plzUrl } from "../utils/crawler.utils";
+import { PLZ, plzUrl } from "../utils/crawler.utils";
 
 export async function crawlHomegate(): Promise<FlatData[] | undefined> {
   const browser: Browser = await launch({ args: ["--no-sandbox"] });
@@ -59,64 +59,97 @@ async function findElements(page: Page): Promise<FlatData[] | undefined> {
     return undefined;
   }
 
-  const results: FlatData[] = elements.map((element: string) => {
-    const dom = new JSDOM(`<!DOCTYPE html><div>${element}</div>`);
+  const results: FlatData[] = elements
+    .filter((element: string) => {
+      const dom = new JSDOM(`<!DOCTYPE html><div>${element}</div>`);
 
-    const link = dom.window.document.querySelector("div > a");
-    const image = dom.window.document.querySelector(
-      'div[class*="ResultlistItemImage"] > img'
-    );
-    const title = dom.window.document.querySelector(
-      'div[class*="DescriptionSimple_description"] > p'
-    );
-    const location = dom.window.document.querySelector(
-      'span[class*="AddressData_address"]'
-    );
-    const rooms = dom.window.document.querySelector(
-      'span[class*="RoomNumber_value"]'
-    );
-    const priceElement = dom.window.document.querySelector(
-      'span[class*="ListingPriceSimple_price"] span:not([class*="ListingPriceSimple_currency"])'
-    );
+      const priceElement = dom.window.document.querySelector(
+        'span[class*="ListingPriceSimple_price"] span:not([class*="ListingPriceSimple_currency"])'
+      );
 
-    if (rooms) {
-      const label = rooms.querySelector('span[class*="RoomNumber_label"]');
+      return priceElement !== undefined && filterPlz(dom);
+    })
+    .map((element: string) => {
+      const dom = new JSDOM(`<!DOCTYPE html><div>${element}</div>`);
 
-      if (label && label.parentElement) {
-        label.parentElement.removeChild(label);
+      const link = dom.window.document.querySelector("div > a");
+      const image = dom.window.document.querySelector(
+        'div[class*="ResultlistItemImage"] > img'
+      );
+      const title = dom.window.document.querySelector(
+        'div[class*="DescriptionSimple_description"] > p'
+      );
+      const location = dom.window.document.querySelector(
+        'span[class*="AddressData_address"]'
+      );
+      const rooms = dom.window.document.querySelector(
+        'span[class*="RoomNumber_value"]'
+      );
+      const priceElement = dom.window.document.querySelector(
+        'span[class*="ListingPriceSimple_price"] span:not([class*="ListingPriceSimple_currency"])'
+      );
+
+      if (rooms) {
+        const label = rooms.querySelector('span[class*="RoomNumber_label"]');
+
+        if (label && label.parentElement) {
+          label.parentElement.removeChild(label);
+        }
       }
-    }
 
-    const price: number =
-      priceElement && priceElement.textContent
-        ? parseFloat(
-            priceElement.textContent.replace("’", "").replace(".–", "")
-          )
-        : 0;
+      const price: number =
+        priceElement && priceElement.textContent
+          ? parseFloat(
+              priceElement.textContent.replace("’", "").replace(".–", "")
+            )
+          : 0;
 
-    const url: string | null =
-      link && link.hasAttribute("href")
-        ? `https://www.homegate.ch/${link.getAttribute("href")}`
-        : null;
+      const url: string | null =
+        link && link.hasAttribute("href")
+          ? `https://www.homegate.ch/${link.getAttribute("href")}`
+          : null;
 
-    return {
-      url: url,
-      image_url: image ? image.getAttribute("src") : null,
-      title:
-        title && title.textContent
-          ? title.textContent.replace("\n", "").trim()
-          : null,
-      location:
-        location && location.textContent ? location.textContent.trim() : null,
-      rooms:
-        rooms && rooms.textContent ? parseFloat(rooms.textContent.trim()) : 0,
-      price: price,
-      published_at: new Date(),
-      source: "homegate"
-    } as FlatData;
-  });
+      return {
+        url: url,
+        image_url: image ? image.getAttribute("src") : null,
+        title:
+          title && title.textContent
+            ? title.textContent.replace("\n", "").trim()
+            : null,
+        location:
+          location && location.textContent ? location.textContent.trim() : null,
+        rooms:
+          rooms && rooms.textContent ? parseFloat(rooms.textContent.trim()) : 0,
+        price: price,
+        published_at: new Date(),
+        source: "homegate"
+      } as FlatData;
+    });
 
   return results;
+}
+
+// Even if the user filter the query, still not related PLZ are delivered
+function filterPlz(dom: JSDOM): boolean {
+  const location = dom.window.document.querySelector(
+    'span[class*="AddressData_address"]'
+  );
+
+  if (!location || !location.textContent) {
+    return false;
+  }
+
+  const info: string[] | null = location.textContent.split(", ");
+
+  if (!info || info.length <= 0) {
+    return false;
+  }
+
+  const plz: number = parseInt(
+    info[info.length > 1 ? 1 : 0].trim().replace(/[^0-9]/g, "")
+  );
+
+  return PLZ.indexOf(plz) >= 0;
 }
 
 // Uncomment to run the function locally
