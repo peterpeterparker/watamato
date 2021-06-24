6b136b72a69718e8724c0f42d61d13306e44b7cb
peterpeterparker <david.dalbusco@outlook.com>
2021-06-24 07:06:31 +0200

    feat: update larger criteria

--- cloud/functions/src/utils/crawler.utils.ts
+++ cloud/functions/src/utils/crawler.utils.ts
@@ -1,8 +1,8 @@
-export const PLZ = [8049, 8037, 8064, 8005, 8006, 8044, 8032, 8008];
+export const PLZ = [8049, 8037, 8064, 8005, 8006, 8044, 8032, 8008, 8004, 8003, 8001, 8002];
 
 export function plzUrl() {
   return encodeURI(PLZ.join(","));
 }
 
 export const priceMin: number = 1300;
-export const priceMax: number = 1800;
+export const priceMax: number = 2000;
